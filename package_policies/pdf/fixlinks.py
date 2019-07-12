65de270d7854af24a850937fa981bb6f6c11ab95
Balos, Cody Joe <balos1@llnl.gov>
2019-07-12 11:08:27 -0700

    fix logo in pdf

--- package_policies/pdf/fixlinks.py
+++ package_policies/pdf/fixlinks.py
@@ -1,18 +1,25 @@
 """
  Convert policy file links to section links in the PDF.
+ Also fix image absolute link(s).
 """
 
 import panflute as pf
 import re
 
 def action(elem, doc):
+    # fix policy links
     if isinstance(elem, pf.Link):
         url = elem.url
         match = re.search('/package_policies/(.+?)\.md', url)
         if match:
-            # fix policy links
             found = match.group(1).lower()
             elem.url = f'#{found}'
+
+    # fix image links with absolute path
+    if isinstance(elem, pf.Image):
+        url = elem.url
+        if url.startswith('/'):
+            elem.url = f'../..{url}'
 
 if __name__ == '__main__':
     pf.run_filter(action)
